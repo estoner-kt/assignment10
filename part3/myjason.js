dd702056b6f3ee4e900fabfc663df66346b98280
Kate Stoner <estoner.kt@gmail.com>
2017-05-02 17:51:37 -0700

    f
    f

---
+++ part3/myjason.js
@@ -0,0 +1,23 @@
+{"teammembers":[
+	{
+		"name":"Valencia",
+		"title":"Vice President of Accounting",
+		"bio":"I wish I can be cosmonaut."
+	},
+	{
+		"name":"Damon",
+		"title":"Director of Development",
+		"bio":"Damon is damon"
+	},
+	{
+		"name":"Chris",
+		"title":"Director of Human Resources",
+		"bio":"Human resources is my favourite"
+	},
+	{
+		"name":"Mike",
+		"title":"Vice President of Sales and Marketing",
+		"bio":"Mike has a corgi named Piper"
+	}
+	
+]}
